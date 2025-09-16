d31774804e4f4aacca2bf6da1d9a4505aeec2b0f
Sulaimanb1012 <9024289@student.tcrmbo.nl>
2025-09-16 13:39:56 +0200

    4 API Opdracht 2

---
+++ 4-api/opdracht-02-countries/app.js
@@ -0,0 +1,28 @@
+fetch('https://restcountries.com/v3.1/all')
+  .then(countries => countries.json())
+  .then(countries => {
+
+
+    const container = document.getElementById('countries');
+    
+    for (let country of countries.data.city) {
+        console.log(countries);
+      const div = document.createElement('div');
+      div.innerHTML = `
+        <h3>${country.name}</h3>
+        <p>Type: ${country.type}</p>
+        <img src="${country.images.large}" alt="${country.name}" width="100">
+      `;
+      div.style.border = '1px solid #ccc';
+      div.style.margin = '10px';
+      div.style.padding = '10px';
+      div.style.borderRadius = '5px';
+      
+      container.appendChild(div);
+    }
+  })
+  .catch(error => {
+    console.log('Fout:', error);
+    document.getElementById('countries').innerHTML =
+      '<p>Sorry het ding kan niet geladen worden.</p>';
+  });
